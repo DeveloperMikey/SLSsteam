7366bedfb8f79eda4f2fa138fb0c86ebdd29bcb1
Ace SLS <acesls@protonmail.com>
2026-08-07 11:56:44 +0200

    style(sdk): Add comment on how to use CCMInterface::recvPkt

--- src/sdk/CCMInterface.hpp
+++ src/sdk/CCMInterface.hpp
@@ -6,6 +6,12 @@
 class CCMInterface
 {
 public:
+	//Before using make sure to:
+	//Create header with steamId & realm
+	//Create body
+	//Serialize
+	//Set type with ProtoBuf mask
+	//Set refs to 1 (not doing this will debugbreak() in a failed assert)
 	void recvPkt(CNetPacket* pkt);
 };
 
